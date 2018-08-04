a81c9a173814be02a3eb0a24cf95cb29b2b715fb
Dylan Kim <mlach9u@gmail.com>
2018-08-04 21:45:04 +0900

    Input operator for [std::pair] is improved and input and output operators for [std::tuple] are added

--- src/col_stream/bstreamTest.cpp
+++ src/col_stream/bstreamTest.cpp
@@ -2,6 +2,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <tuple>
+
 #include <fstream>
 #include "bstream_operator.h"
 
@@ -26,55 +32,23 @@ void read(const std::basic_string< _Elem >& _Name, _Ty& _Data)
 int main(int argc, char* argv[])
 {
 	{
-		std::map< std::string, std::vector< std::string > > data;
-
-		{
-			std::vector< std::string > vps;
-			vps.push_back((std::string("1_1")));
-			vps.push_back((std::string("1_2")));
-			vps.push_back((std::string("1_3")));
-			vps.push_back((std::string("1_4")));
-			vps.push_back((std::string("1_5")));
-			data[std::string("vps1")] = vps;
-		}
-
-		{
-			std::vector< std::string > vps;
-			vps.push_back((std::string("2_1")));
-			vps.push_back((std::string("2_2")));
-			vps.push_back((std::string("2_3")));
-			vps.push_back((std::string("2_4")));
-			vps.push_back((std::string("2_5")));
-			data[std::string("vps2")] = vps;
-		}
-
-		{
-			std::vector< std::string > vps;
-			vps.push_back((std::string("3_1")));
-			vps.push_back((std::string("3_2")));
-			vps.push_back((std::string("3_3")));
-			vps.push_back((std::string("3_4")));
-			vps.push_back((std::string("3_5")));
-			data[std::string("vps3")] = vps;
-		}
-
-		{
-			std::vector< std::string > vps;
-			vps.push_back((std::string("4_1")));
-			vps.push_back((std::string("4_2")));
-			vps.push_back((std::string("4_3")));
-			vps.push_back((std::string("4_4")));
-			vps.push_back((std::string("4_5")));
-			data[std::string("vps4")] = vps;
-		}
+		std::tuple< int, double, std::string,
+			std::vector< float >,
+			std::list< std::pair< int, double > >,
+			std::map< std::string, std::string >
+		> data;
 
 		write(std::string("filename.ext"), data);
 	}
 
 	{
-		std::map< std::string, std::vector< std::string > > data;
+		std::tuple< int, double, std::string,
+			std::vector< float >,
+			std::list< std::pair< int, double > >,
+			std::map< std::string, std::string >
+		> data;
+
 		read(std::string("filename.ext"), data);
-		write(std::string("filename2.ext"), data);
 	}
 
 	return 0;
--- src/col_stream/bstream_operator.h
+++ src/col_stream/bstream_operator.h
@@ -3,10 +3,7 @@
 
 #include "bstream.h"
 
-#include <vector>
-#include <list>
-#include <map>
-
+#ifdef _XSTRING_
 template< typename _Elem, typename _Traits, typename _Alloc >
 basic_bostream< _Elem, _Traits >& operator << (basic_bostream< _Elem, _Traits >& _Ostr, const std::basic_string< _Elem, _Traits, _Alloc >& _Str)
 {
@@ -35,7 +32,9 @@ basic_bistream< _Elem, _Traits >& operator >> (basic_bistream< _Elem, _Traits >&
 
 	return _Istr;
 }
+#endif
 
+#ifdef _VECTOR_
 template< typename _Elem, typename _Traits, typename _Ty >
 basic_bostream< _Elem, _Traits >& operator << (basic_bostream< _Elem, _Traits >& _Ostr, const std::vector< _Ty >& _Tys)
 {
@@ -65,7 +64,9 @@ basic_bistream< _Elem, _Traits >& operator >> (basic_bistream< _Elem, _Traits >&
 
 	return _Istr;
 }
+#endif
 
+#ifdef _LIST_
 template< typename _Elem, typename _Traits, typename _Ty >
 basic_bostream< _Elem, _Traits >& operator << (basic_bostream< _Elem, _Traits >& _Ostr, const std::list< _Ty >& _Tys)
 {
@@ -95,7 +96,9 @@ basic_bistream< _Elem, _Traits >& operator >> (basic_bistream< _Elem, _Traits >&
 
 	return _Istr;
 }
+#endif
 
+#ifdef _UTILITY_
 template< typename _Elem, typename _Traits, typename _Ty1, typename _Ty2 >
 basic_bostream< _Elem, _Traits >& operator << (basic_bostream< _Elem, _Traits >& _Ostr, const std::pair< _Ty1, _Ty2 >& _Pair)
 {
@@ -104,12 +107,14 @@ basic_bostream< _Elem, _Traits >& operator << (basic_bostream< _Elem, _Traits >&
 }
 
 template< typename _Elem, typename _Traits, typename _Ty1, typename _Ty2 >
-basic_bistream< _Elem, _Traits >& operator << (basic_bistream< _Elem, _Traits >& _Istr, const std::pair< _Ty1, _Ty2 >& _Pair)
+basic_bistream< _Elem, _Traits >& operator >> (basic_bistream< _Elem, _Traits >& _Istr, std::pair< _Ty1, _Ty2 >& _Pair)
 {
 	_Istr >> _Pair.first >> _Pair.second;
 	return _Istr;
 }
+#endif
 
+#ifdef _MAP_
 template< typename _Elem, typename _Traits, typename _Kty, typename _Ty, typename _Pr, typename _Alloc >
 basic_bostream< _Elem, _Traits >& operator << (basic_bostream< _Elem, _Traits >& _Ostr, const std::map< _Kty, _Ty, _Pr, _Alloc >& _Map)
 {
@@ -140,5 +145,34 @@ basic_bistream< _Elem, _Traits >& operator >> (basic_bistream< _Elem, _Traits >&
 
 	return _Istr;
 }
+#endif
+
+#ifdef _TUPLE_
+template< typename _Elem, typename _Traits >
+basic_bostream< _Elem, _Traits >& operator << (basic_bostream< _Elem, _Traits >& _Ostr, const std::tuple<>&)
+{
+	return _Ostr;
+}
+
+template< typename _Elem, typename _Traits >
+basic_bistream< _Elem, _Traits >& operator >> (basic_bistream< _Elem, _Traits >& _Istr, std::tuple<>&)
+{
+	return _Istr;
+}
+
+template< typename _Elem, typename _Traits, typename _This, typename... _Rest >
+basic_bostream< _Elem, _Traits >& operator << (basic_bostream< _Elem, _Traits >& _Ostr, const std::tuple< _This, _Rest... >& _Tuple)
+{
+	_Ostr << _Tuple._Myfirst._Val << _Tuple._Get_rest();
+	return _Ostr;
+}
+
+template< typename _Elem, typename _Traits, typename _This, typename... _Rest >
+basic_bistream< _Elem, _Traits >& operator >> (basic_bistream< _Elem, _Traits >& _Istr, std::tuple< _This, _Rest... >& _Tuple)
+{
+	_Istr >> _Tuple._Myfirst._Val >> _Tuple._Get_rest();
+	return _Istr;
+}
+#endif
 
 #endif
